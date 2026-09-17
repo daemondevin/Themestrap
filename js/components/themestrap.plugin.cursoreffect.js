e2dbe7a65326b39d8341fdac773f309750e5f703
daemondevin <you@example.com>
2026-09-16 19:10:29 -0700

    Add demo pages for Toast and Toggle plugins
    - Created `plugin.demo.toast.html` to showcase the Toast plugin with various types, positions, and a static API.
    - Created `plugin.demo.toggle.html` to demonstrate the Toggle plugin with default, accordion, active start, and preview paragraph features.

--- js/components/themestrap.plugin.cursoreffect.js
+++ js/components/themestrap.plugin.cursoreffect.js
@@ -1,15 +1,16 @@
+
 // CursorEffect
 (((themestrap = {}, $) => {
-    const instanceName = '__cursorEffect';
+	const instanceName = '__cursorEffect';
 
-    class PluginCursorEffect {
-        constructor($el, opts) {
-            return this.initialize($el, opts);
-        }
+	class PluginCursorEffect {
+		constructor($el, opts) {
+			return this.initialize($el, opts);
+		}
 
-        initialize($el, opts) {
+		initialize($el, opts) {
 			if ($el.data(instanceName)) {
-				return this;
+				return $el.data(instanceName);
 			}
 
 			this.$el = $el;
@@ -23,13 +24,13 @@
 			return this;
 		}
 
-        setData() {
+		setData() {
 			this.$el.data(instanceName, this);
 
 			return this;
 		}
 
-        setOptions(opts) {
+		setOptions(opts) {
 			this.options = $.extend(true, {}, PluginCursorEffect.defaults, opts, {
 				wrapper: this.$el
 			});
@@ -37,103 +38,188 @@
 			return this;
 		}
 
-        build() {
+		build() {
 			const self = this;
 
-			// Global Variables for cursor position
 			self.clientX = -100;
 			self.clientY = -100;
+			self.isStuck = false;
+			self.isVisible = true;
 
-			// Hide Mouse Cursor
-			if( self.options.hideMouseCursor ) {
+			// Hide native mouse cursor.
+			if (self.options.hideMouseCursor) {
 				self.$el.addClass('hide-mouse-cursor');
 			}
 
-			// Creates the cursor wrapper node
-			const cursorOuter = document.createElement('DIV');
-				cursorOuter.className = 'cursor-outer';
+			// Creates the cursor outer node.
+			self.cursorOuter = document.createElement('div');
+			self.cursorOuter.className = 'cursor-outer';
 
-			// Creates the cursor inner node
-			const cursorInner = document.createElement('DIV');
-				cursorInner.className = 'cursor-inner';
+			// Creates the cursor inner node.
+			self.cursorInner = document.createElement('div');
+			self.cursorInner.className = 'cursor-inner';
 
-			// Custom Cursor Outer Color
-			if( self.options.cursorOuterColor ) {
-				cursorOuter.style = 'border-color: ' + self.options.cursorOuterColor + ';';
+			// Custom cursor outer color.
+			if (self.options.cursorOuterColor) {
+				self.cursorOuter.style.borderColor = self.options.cursorOuterColor;
 			}
 
-			// Custom Cursor Inner Color
-			if( self.options.cursorInnerColor ) {
-				cursorInner.style = 'background-color: ' + self.options.cursorInnerColor + ';';
+			// Custom cursor inner color.
+			if (self.options.cursorInnerColor) {
+				self.cursorInner.style.backgroundColor = self.options.cursorInnerColor;
 			}
 
-			// Size
-			if( self.options.size ) {
-				switch ( self.options.size ) {
-					case 'small':
-						self.$el.addClass( 'cursor-effect-size-small' );
-						break;
-					
-					case 'big':
-						self.$el.addClass( 'cursor-effect-size-big' );
-						break;
-				}
+			// Size.
+			self.applySize();
+
+			// Style.
+			if (self.options.style) {
+				self.$el.addClass(self.options.style);
 			}
 
-			// Style
-			if( self.options.style ) {
-				self.$el.addClass( self.options.style );
-			}
+			// Add cursor nodes to the document.
+			document.body.prepend(self.cursorOuter);
+			document.body.prepend(self.cursorInner);
 
-			// Prepend cursor wrapper node to the body
-			document.body.prepend( cursorOuter );
-
-			// Prepend cursor inner node to the body
-			document.body.prepend( cursorInner );
-
-			// Loop for render
+			// Render loop.
 			const render = () => {
-				cursorOuter.style.transform = 'translate('+ self.clientX +'px, '+ self.clientY +'px)';
-				cursorInner.style.transform = 'translate('+ self.clientX +'px, '+ self.clientY +'px)';
+				if (!self.cursorOuter || !self.cursorInner) {
+					return;
+				}
+
+				self.cursorOuter.style.transform =
+					'translate(' + self.clientX + 'px, ' + self.clientY + 'px)';
+
+				self.cursorInner.style.transform =
+					'translate(' + self.clientX + 'px, ' + self.clientY + 'px)';
 
 				self.loopInside = requestAnimationFrame(render);
 			};
+
 			self.loop = requestAnimationFrame(render);
 
 			return this;
 		}
 
-        events() {
-			const self = this, $cursorOuter = $('.cursor-outer'), $cursorInner = $('.cursor-inner');
+		applySize() {
+			const self = this;
 
-			const initialCursorOuterBox    = $cursorOuter[0].getBoundingClientRect(), initialCursorOuterRadius = $cursorOuter.css('border-radius');
+			self.$el.removeClass(
+				'cursor-effect-size-small cursor-effect-size-big'
+			);
 
-			// Update Cursor Position
-			document.addEventListener('mousemove', ({clientX, clientY}) => {
-				if( !self.isStuck ) {
+			switch (self.options.size) {
+				case 'small':
+					self.$el.addClass('cursor-effect-size-small');
+					break;
+
+				case 'big':
+					self.$el.addClass('cursor-effect-size-big');
+					break;
+			}
+
+			return this;
+		}
+
+		setSize(size) {
+			const allowedSizes = ['small', 'default', 'big'];
+
+			if (allowedSizes.indexOf(size) === -1) {
+				size = 'default';
+			}
+
+			this.options.size = size;
+			this.applySize();
+
+			return this;
+		}
+
+		show() {
+			const self = this;
+
+			if (self.cursorOuter) {
+				$(self.cursorOuter).removeClass('opacity-0');
+			}
+
+			if (self.cursorInner) {
+				$(self.cursorInner).removeClass('opacity-0');
+			}
+
+			self.isVisible = true;
+
+			return this;
+		}
+
+		hide() {
+			const self = this;
+
+			if (self.cursorOuter) {
+				$(self.cursorOuter).addClass('opacity-0');
+			}
+
+			if (self.cursorInner) {
+				$(self.cursorInner).addClass('opacity-0');
+			}
+
+			self.isVisible = false;
+
+			return this;
+		}
+
+		events() {
+			const self = this;
+			const $cursorOuter = $(self.cursorOuter);
+			const $cursorInner = $(self.cursorInner);
+
+			const initialCursorOuterBox =
+				self.cursorOuter.getBoundingClientRect();
+
+			const initialCursorOuterRadius =
+				$cursorOuter.css('border-radius');
+
+			// Update cursor position.
+			self._mousemoveHandler = ({ clientX, clientY }) => {
+				if (!self.isStuck) {
 					self.clientX = clientX - 20;
 					self.clientY = clientY - 20;
 				}
 
-				$cursorOuter.removeClass('opacity-0');
-			});
+				if (self.isVisible) {
+					$cursorOuter.removeClass('opacity-0');
+				}
+			};
 
-			self.isStuck = false;
-			$('[data-cursor-effect-hover]').on('mouseenter', function(e){
+			document.addEventListener(
+				'mousemove',
+				self._mousemoveHandler
+			);
 
-				// Identify Event With Hover Class
+			// Hover effects.
+			self._hoverSelector = '[data-cursor-effect-hover]';
+
+			self._mouseenterHandler = function () {
+				const $this = $(this);
+
 				$cursorOuter.addClass('cursor-outer-hover');
 				$cursorInner.addClass('cursor-inner-hover');
 
-				// Hover Color
-				const hoverColor = $(this).data('cursor-effect-hover-color');
-				$cursorOuter.addClass( 'cursor-color-' + hoverColor );
-				$cursorInner.addClass( 'cursor-color-' + hoverColor );
+				const hoverColor =
+					$this.data('cursor-effect-hover-color');
 
-				// Effect Types
-				switch ( $(this).data('cursor-effect-hover') ) {
-					case 'fit':
-						const thisBox = $(this)[0].getBoundingClientRect();
+				if (hoverColor) {
+					$cursorOuter.addClass(
+						'cursor-color-' + hoverColor
+					);
+
+					$cursorInner.addClass(
+						'cursor-color-' + hoverColor
+					);
+				}
+
+				switch ($this.data('cursor-effect-hover')) {
+					case 'fit': {
+						const thisBox =
+							$this[0].getBoundingClientRect();
 
 						self.clientX = thisBox.x;
 						self.clientY = thisBox.y;
@@ -141,33 +227,41 @@
 						$cursorOuter.css({
 							width: thisBox.width,
 							height: thisBox.height,
-							'border-radius': $(this).css('border-radius')
+							'border-radius': $this.css('border-radius')
 						}).addClass('cursor-outer-fit');
 
 						$cursorInner.addClass('opacity-0');
 
 						self.isStuck = true;
 						break;
+					}
 
 					case 'plus':
 						$cursorInner.addClass('cursor-inner-plus');
 						break;
 				}
-			});
+			};
 
-			$('[data-cursor-effect-hover]').on('mouseleave', function(){
-				
-				// Identify Event With Hover Class
+			self._mouseleaveHandler = function () {
+				const $this = $(this);
+
 				$cursorOuter.removeClass('cursor-outer-hover');
 				$cursorInner.removeClass('cursor-inner-hover');
 
-				// Remove Color Class
-				const hoverColor = $(this).data('cursor-effect-hover-color');
-				$cursorOuter.removeClass( 'cursor-color-' + hoverColor );
-				$cursorInner.removeClass( 'cursor-color-' + hoverColor );
+				const hoverColor =
+					$this.data('cursor-effect-hover-color');
 
-				// Effect Types
-				switch ( $(this).data('cursor-effect-hover') ) {
+				if (hoverColor) {
+					$cursorOuter.removeClass(
+						'cursor-color-' + hoverColor
+					);
+
+					$cursorInner.removeClass(
+						'cursor-color-' + hoverColor
+					);
+				}
+
+				switch ($this.data('cursor-effect-hover')) {
 					case 'fit':
 						$cursorOuter.css({
 							width: initialCursorOuterBox.width,
@@ -181,53 +275,118 @@
 						break;
 
 					case 'plus':
-						$cursorInner.removeClass('cursor-inner-plus');
+						$cursorInner.removeClass(
+							'cursor-inner-plus'
+						);
 						break;
 				}
-			});
+			};
 
-			$(window).on('scroll', () => {
-				if( $cursorOuter.hasClass('cursor-outer-fit') ) {
-					$cursorOuter.addClass('opacity-0').removeClass('cursor-outer-fit');
+			$(document)
+				.on(
+					'mouseenter' + instanceName,
+					self._hoverSelector,
+					self._mouseenterHandler
+				)
+				.on(
+					'mouseleave' + instanceName,
+					self._hoverSelector,
+					self._mouseleaveHandler
+				);
+
+			// If the cursor is stuck to an element, release it on scroll.
+			self._scrollHandler = () => {
+				if ($cursorOuter.hasClass('cursor-outer-fit')) {
+					$cursorOuter
+						.addClass('opacity-0')
+						.removeClass('cursor-outer-fit');
+
+					$cursorInner.removeClass('opacity-0');
+
+					self.isStuck = false;
 				}
-			});
+			};
+
+			$(window).on(
+				'scroll' + instanceName,
+				self._scrollHandler
+			);
 
 			return this;
 		}
 
-        destroy() {
+		destroy() {
 			const self = this;
 
-			self.$el.removeClass('hide-mouse-cursor cursor-effect-size-small cursor-effect-size-big cursor-effect-style-square');		
+			// Stop animation loop.
+			if (self.loop) {
+				cancelAnimationFrame(self.loop);
+			}
 
-			cancelAnimationFrame( self.loop );
-			cancelAnimationFrame( self.loopInside );
+			if (self.loopInside) {
+				cancelAnimationFrame(self.loopInside);
+			}
 
-			document.querySelector('.cursor-outer').remove();
-			document.querySelector('.cursor-inner').remove();
+			// Remove document events.
+			if (self._mousemoveHandler) {
+				document.removeEventListener(
+					'mousemove',
+					self._mousemoveHandler
+				);
+			}
 
-			self.$el.removeData( instanceName, self );
+			$(document).off(instanceName);
+			$(window).off(instanceName);
+
+			// Restore host element.
+			self.$el.removeClass(
+				'hide-mouse-cursor ' +
+				'cursor-effect-size-small ' +
+				'cursor-effect-size-big ' +
+				'cursor-effect-style-square'
+			);
+
+			// Remove cursor elements.
+			if (self.cursorOuter) {
+				self.cursorOuter.remove();
+			}
+
+			if (self.cursorInner) {
+				self.cursorInner.remove();
+			}
+
+			// Remove stored instance.
+			self.$el.removeData(instanceName);
+
+			self.cursorOuter = null;
+			self.cursorInner = null;
+
+			return this;
 		}
-    }
-
-    PluginCursorEffect.defaults = {
-
 	}
 
-    $.extend(themestrap, {
+	PluginCursorEffect.defaults = {
+		size: 'default',
+		hideMouseCursor: false,
+		cursorOuterColor: null,
+		cursorInnerColor: null,
+		style: null
+	};
+
+	$.extend(themestrap, {
 		PluginCursorEffect
 	});
 
-    $.fn.themestrapPluginCursorEffect = function(opts) {
-		return this.map(function() {
+	$.fn.themestrapPluginCursorEffect = function(opts) {
+		return this.map(function () {
 			const $this = $(this);
 
 			if ($this.data(instanceName)) {
 				return $this.data(instanceName);
-			} else {
-				return new PluginCursorEffect($this, opts);
 			}
 
+			return new PluginCursorEffect($this, opts);
 		});
-	}
+	};
+
 })).apply(this, [window.themestrap, jQuery]);
--- js/components/themestrap.plugin.floatelement.js
+++ js/components/themestrap.plugin.floatelement.js
@@ -2,157 +2,330 @@
 (((themestrap = {}, $) => {
     const instanceName = '__floatElement';
 
+    let _instanceCounter = 0;
+
+    function lerp(a, b, t) {
+        return a + (b - a) * Math.max(0, Math.min(1, t));
+    }
+
     class PluginFloatElement {
         constructor($el, opts) {
             return this.initialize($el, opts);
         }
 
         initialize($el, opts) {
-			if ($el.data(instanceName)) {
-				return this;
-			}
+            if ($el.data(instanceName)) {
+                return this;
+            }
 
-			this.$el = $el;
+            this.$el         = $el;
+            this._uid        = ++_instanceCounter;
+            this._inView     = false;
+            this._paused     = false;
+            this._ticking    = false;
+            this._rafId      = null;
+            this._active     = false;
+            this._lastScrollY = window.scrollY || 0;
 
-			this
-				.setData()
-				.setOptions(opts)
-				.build();
-
-			return this;
-		}
-
-        setData() {
-			this.$el.data(instanceName, this);
-
-			return this;
-		}
-
-        setOptions(opts) {
-			this.options = $.extend(true, {}, PluginFloatElement.defaults, opts, {
-				wrapper: this.$el
-			});
-
-			return this;
-		}
-
-        build() {
-            const self = this;
-            const $el = this.options.wrapper;
-            const $window = $(window);
-            let minus;
-
-            // If has floating elements inside a SVG. 
-            // Intersection Observer API do not check elements inside SVG's, so we need initialize trough top parent SVG
-            if( $el.data('plugin-float-element-svg') ) {
-				$el.find('[data-plugin-float-element]').each(function(){
-                    const $this = $(this);
-                    let opts;
-
-                    const pluginOptions = themestrap.fn.getOptions($this.data('plugin-options'));
-                    if (pluginOptions)
-						opts = pluginOptions;
-
-                    $this.themestrapPluginFloatElement(opts);
-                });
-
-				return this;
-			}
-
-            if( self.options.style ) {
-				$el.attr('style', self.options.style);
-			}
-
-            if( $window.width() > self.options.minWindowWidth ) {
-
-				// Set Start Position
-				if( self.options.startPos == 'none' ) {
-					minus = '';
-				} else if( self.options.startPos == 'top' ) {
-					$el.css({
-						top: 0
-					});
-					minus = '';
-				} else {
-					$el.css({
-						bottom: 0
-					});
-					minus = '-';
-				}
-
-				// Set Transition
-				if( self.options.transition ) {
-					$el.css({
-						transition: 'ease-out transform '+ self.options.transitionDuration +'ms ' + self.options.transitionDelay + 'ms'
-					});
-				}
-
-				// First Load
-				self.movement(minus);	
-
-				// Scroll
-				$window.on('scroll', () => {
-					self.movement(minus);				   
-				});
-
-			}
+            this
+                .setData()
+                .setOptions(opts)
+                .build()
+                .events();
 
             return this;
         }
 
-        movement(minus) {
-			const self = this, $el = this.options.wrapper, $window = $(window), scrollTop = $window.scrollTop(), elementOffset = $el.offset().top, currentElementOffset = (elementOffset - scrollTop), factor = ( self.options.isInsideSVG ) ? 2 : 100;
+        setData() {
+            this.$el.data(instanceName, this);
+            return this;
+        }
 
-		   	const scrollPercent = factor * currentElementOffset / ($window.height());
+        setOptions(opts) {
+            this.options = $.extend(true, {}, PluginFloatElement.defaults, opts, {
+                wrapper: this.$el
+            });
+            return this;
+        }
 
-		   	if( $el.visible( true ) ) {
+        build() {
+            const self = this;
+            const opts = self.options;
+            const $el  = opts.wrapper;
 
-		   		if( !self.options.horizontal ) {
+            /* SVG child delegation — IO cannot observe inside SVGs so the parent element
+               gets data-plugin-float-element-svg and delegates to each child */
+            if ($el.data('plugin-float-element-svg')) {
+                $el.find('[data-plugin-float-element]').each(function () {
+                    const $child     = $(this);
+                    const childOpts  = themestrap.fn.getOptions($child.data('plugin-options'));
+                    $child.themestrapPluginFloatElement(childOpts || undefined);
+                });
+                return this;
+            }
 
-		   			$el.css({
-			   			transform: 'translate3d(0, '+ minus + scrollPercent / self.options.speed +'%, 0)'
-			   		});
+            /* Respect prefers-reduced-motion */
+            if (opts.respectReducedMotion &&
+                window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+                return this;
+            }
 
-		   		} else {
+            /* Skip on narrow viewports */
+            if ($(window).width() <= opts.minWindowWidth) {
+                return this;
+            }
 
-		   			$el.css({
-			   			transform: 'translate3d('+ minus + scrollPercent / self.options.speed +'%, 0, 0)'
-			   		});
+            /* Legacy arbitrary style override */
+            if (opts.style) {
+                $el.attr('style', opts.style);
+            }
 
-		   		}
-		   		
-		   	}
+            /* CSS positioning for absolutely-placed decorative elements */
+            if (opts.startPos === 'top') {
+                $el.css({ top: '0px' });
+            } else if (opts.startPos === 'bottom') {
+                $el.css({ bottom: '0px' });
+            }
 
-		}
+            /* will-change hint for GPU compositing */
+            $el.css('will-change', 'transform');
+
+            /* Optional CSS transition to smooth between scroll ticks */
+            if (opts.transition) {
+                const dur   = opts.transitionDuration + 'ms';
+                const delay = opts.transitionDelay   + 'ms';
+                let   tx    = 'transform ' + dur + ' ease-out ' + delay;
+                if (opts.opacity !== false) {
+                    tx += ', opacity ' + dur + ' ease-out ' + delay;
+                }
+                $el.css('transition', tx);
+            }
+
+            /* IntersectionObserver — sets _inView without polling */
+            if ('IntersectionObserver' in window) {
+                self._observer = new IntersectionObserver(entries => {
+                    self._inView = entries[0].isIntersecting;
+                }, { rootMargin: '20% 0px 20% 0px' });
+                self._observer.observe($el[0]);
+            } else {
+                self._inView = true;
+            }
+
+            self._active = true;
+
+            /* Seed position on build so there is no pop on first scroll */
+            self._move();
+
+            return this;
+        }
+
+        events() {
+            const self = this;
+
+            if (!self._active) return this;
+
+            $(window).on('scroll.floatelement.' + self._uid, () => {
+                if (self._paused || self._ticking) return;
+                self._ticking = true;
+                self._rafId = requestAnimationFrame(() => {
+                    self._move();
+                    self._ticking = false;
+                });
+            });
+
+            return this;
+        }
+
+        /* Compute all transforms and apply them */
+        _move() {
+            const self = this;
+            const opts = self.options;
+            const $el  = opts.wrapper;
+
+            if (!self._inView) return;
+
+            /* Scroll-direction gate */
+            const currentScrollY = window.scrollY || $(window).scrollTop();
+            const scrollingDown  = currentScrollY >= self._lastScrollY;
+            self._lastScrollY    = currentScrollY;
+
+            if (opts.scrollDirection === 'down' && !scrollingDown) return;
+            if (opts.scrollDirection === 'up'   &&  scrollingDown) return;
+
+            /* scrollPercent — 100 at the element's natural top,
+               falling as the user scrolls the element into view.
+               The SVG factor keeps animated SVG children in a tighter range. */
+            const factor      = opts.isInsideSVG ? 2 : 100;
+            const $win        = $(window);
+            const elTop       = $el.offset().top;
+            const winTop      = $win.scrollTop();
+            const scrollPct   = factor * (elTop - winTop) / $win.height();
+
+            /* Normalized 0–1 for interpolated transforms */
+            const t           = Math.max(0, Math.min(1, scrollPct / 100));
+
+            /* Direction: startPos:'bottom' historically inverted the sign;
+               invertDirection flips whatever the base direction is */
+            const baseDir     = opts.startPos === 'bottom' ? -1 : 1;
+            const dir         = opts.invertDirection ? -baseDir : baseDir;
+
+            /* Primary translation */
+            let translateVal  = dir * scrollPct / opts.speed;
+            if (opts.clamp !== false) {
+                const cap    = Math.abs(opts.clamp);
+                translateVal = Math.max(-cap, Math.min(cap, translateVal));
+            }
+
+            /* Build transform string */
+            const transforms = [];
+            const result     = {};
+
+            if (!opts.horizontal) {
+                transforms.push('translate3d(0, ' + translateVal + '%, 0)');
+                result.translateY = translateVal;
+            } else {
+                transforms.push('translate3d(' + translateVal + '%, 0, 0)');
+                result.translateX = translateVal;
+            }
+
+            if (Array.isArray(opts.scale)) {
+                const scaleVal = lerp(opts.scale[0], opts.scale[1], t);
+                transforms.push('scale(' + scaleVal + ')');
+                result.scale = scaleVal;
+            }
+
+            if (Array.isArray(opts.rotate)) {
+                const rotateVal = lerp(opts.rotate[0], opts.rotate[1], t);
+                transforms.push('rotate(' + rotateVal + 'deg)');
+                result.rotate = rotateVal;
+            }
+
+            if (Array.isArray(opts.skew)) {
+                const skewVal = lerp(opts.skew[0], opts.skew[1], t);
+                transforms.push('skewX(' + skewVal + 'deg)');
+                result.skew = skewVal;
+            }
+
+            $el.css('transform', transforms.join(' '));
+
+            /* Opacity */
+            if (Array.isArray(opts.opacity)) {
+                const opacityVal = lerp(opts.opacity[0], opts.opacity[1], t);
+                $el.css('opacity', opacityVal);
+                result.opacity = opacityVal;
+            }
+
+            if (typeof opts.onMove === 'function') {
+                opts.onMove.call($el[0], scrollPct, result);
+            }
+        }
+
+        /* Public API */
+
+        pause() {
+            this._paused = true;
+            return this;
+        }
+
+        resume() {
+            this._paused = false;
+            this._move();
+            return this;
+        }
+
+        recalculate() {
+            this._move();
+            return this;
+        }
+
+        destroy() {
+            const self = this;
+            const $el  = self.options.wrapper;
+
+            $(window).off('scroll.floatelement.' + self._uid);
+
+            if (self._rafId !== null) {
+                cancelAnimationFrame(self._rafId);
+                self._rafId = null;
+            }
+
+            if (self._observer) {
+                self._observer.disconnect();
+                self._observer = null;
+            }
+
+            $el.css({
+                transform:    '',
+                transition:   '',
+                opacity:      '',
+                'will-change': '',
+                top:          '',
+                bottom:       '',
+            });
+
+            $el.removeData(instanceName);
+
+            return this;
+        }
     }
 
     PluginFloatElement.defaults = {
-		startPos: 'top',
-		speed: 3,
-		horizontal: false,
-		isInsideSVG: false,
-		transition: false,
-		transitionDelay: 0,
-		transitionDuration: 500,
-		minWindowWidth: 991
-	};
+        /* Movement */
+        speed:                3,        // divisor — larger value = subtler movement
+        horizontal:           false,    // true → translate on X axis instead of Y
+        invertDirection:      false,    // flip the translation direction
+        startPos:             'top',    // 'top' | 'bottom' | 'none' — sets initial CSS position on the element
 
-    // expose to scope
+        /* Viewport gate */
+        minWindowWidth:       991,      // plugin is inactive below this viewport width (px)
+
+        /* Additional transforms — each accepts false or a [from, to] tuple,
+           interpolated 0–1 across the normalised scroll position */
+        opacity:              false,    // e.g. [1, 0]  — fade out as element scrolls up
+        scale:                false,    // e.g. [1, 1.2]
+        rotate:               false,    // e.g. [0, 30] (degrees)
+        skew:                 false,    // e.g. [0, 8]  (degrees, applied as skewX)
+
+        /* Clamping */
+        clamp:                false,    // max absolute translation in %; false = unlimited
+
+        /* Smoothing */
+        transition:           false,    // add a CSS transition to ease between ticks
+        transitionDuration:   500,      // ms
+        transitionDelay:      0,        // ms
+
+        /* Scroll direction filter */
+        scrollDirection:      'both',   // 'both' | 'down' | 'up'
+
+        /* Accessibility */
+        respectReducedMotion: true,     // honour prefers-reduced-motion
+
+        /* SVG child delegation */
+        isInsideSVG:          false,
+        style:                null,     // arbitrary inline style applied before plugin runs
+
+        /* Callback */
+        onMove:               null,     // (scrollPct: number, transforms: object) => void
+    };
+
     $.extend(themestrap, {
-		PluginFloatElement
-	});
+        PluginFloatElement
+    });
 
-    // jquery plugin
     $.fn.themestrapPluginFloatElement = function(opts) {
-		return this.map(function() {
-			const $this = $(this);
+        return this.map(function() {
+            const $this = $(this);
+            const inst  = $this.data(instanceName);
 
-			if ($this.data(instanceName)) {
-				return $this.data(instanceName);
-			} else {
-				return new PluginFloatElement($this, opts);
-			}
+            /* String command — e.g. $el.themestrapPluginFloatElement('pause') */
+            if (typeof opts === 'string') {
+                if (inst && typeof inst[opts] === 'function') {
+                    inst[opts]();
+                }
+                return $this;
+            }
 
-		});
-	}
+            return inst ? inst : new PluginFloatElement($this, opts);
+        });
+    };
 })).apply(this, [window.themestrap, jQuery]);
